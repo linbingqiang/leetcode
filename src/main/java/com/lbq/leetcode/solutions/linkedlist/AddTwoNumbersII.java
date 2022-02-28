46428df1d3535b48c946e725bce260a949eaec31
linbingqiang <bingqiang.lin1@gmail.com>
2022-02-28 23:51:04 +0800

    2. 链表相加II，不使用链表翻转，使用栈的方式实现

--- src/main/java/com/lbq/leetcode/solutions/linkedlist/AddTwoNumbersII.java
+++ src/main/java/com/lbq/leetcode/solutions/linkedlist/AddTwoNumbersII.java
@@ -1,5 +1,8 @@
 package com.lbq.leetcode.solutions.linkedlist;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 /**
  * @author linbingqiang
  * @since 2022/2/28 11:24 下午
@@ -24,6 +27,33 @@ public class AddTwoNumbersII {
         AddTwoNumbersII addTwoNumbersII = new AddTwoNumbersII();
         ListNode head = addTwoNumbersII.addTwoNumbers(l1, l2);
         LinkedListUtils.printLinkedList(head);
+    }
+
+    public ListNode addTwoNumbersII(ListNode l1, ListNode l2) {
+        Deque<Integer> stack1 = new LinkedList<>();
+        Deque<Integer> stack2 = new LinkedList<>();
+        while (l1 != null) {
+            stack1.push(l1.val);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2.val);
+            l2 = l2.next;
+        }
+        int carry = 0;
+        ListNode ans = null;
+        //使用头插法
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int a = stack1.isEmpty() ? 0 : stack1.pop();
+            int b = stack2.isEmpty() ? 0 : stack2.pop();
+            int cur = a + b + carry;
+            carry = cur / 10;
+            cur = cur % 10;
+            ListNode node = new ListNode(cur);
+            node.next = ans;
+            ans = node;
+        }
+        return ans;
     }
 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
