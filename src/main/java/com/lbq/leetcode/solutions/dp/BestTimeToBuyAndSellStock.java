7e28934dafaf2767da32a55b563386f0f2b1c36b
linbingqiang <linbingqiang@meitaun.com>
2022-03-15 09:14:44 +0800

    买卖股票的最佳时机，dp解法

--- src/main/java/com/lbq/leetcode/solutions/dp/BestTimeToBuyAndSellStock.java
+++ src/main/java/com/lbq/leetcode/solutions/dp/BestTimeToBuyAndSellStock.java
@@ -12,7 +12,7 @@ public class BestTimeToBuyAndSellStock {
         BestTimeToBuyAndSellStock bs = new BestTimeToBuyAndSellStock();
         System.out.println(bs.maxProfit(prices));
         System.out.println(bs.maxProfit1(prices));
-
+        System.out.println(bs.maxProfitDp(prices));
     }
 
 
@@ -36,5 +36,41 @@ public class BestTimeToBuyAndSellStock {
             result = Math.max(result, prices[i] - low);
         }
         return result;
+    }
+
+    public int maxProfitDp(int[] prices) {
+        /**
+         * 1. 确定dp数组的含义及其下标的含义
+         *    dp[i][0]表示第i天持有股票所得的最高现金
+         *    dp[i][1]表示第i天不持有股票所得的最高现金
+         * 2. 确定递推公式
+         *   对于dp[i][0]有两种情况：
+         *   1) 当天不做任何操作，那么第i天所持有股票的最高现金就是第i-1天所持有股票的最高现金
+         *   2) 当天进行股票买入操作，那么第i天所持有股票的最高现金就是第i-1天所持有股票的最高现金减去当天买入股票的价格
+         *   综上所述：
+         *   dp[i][0] = Math.max(dp[i - 1][0],  -prices[i])
+         *   对于dp[i][1]同样也有两种情况:
+         *   1) 当天不做任何操作，那么第i天所持有股票的最高现金就是保持现状，即dp[i][1] = dp[i - 1][1]
+         *   2) 当天进行股票卖出操作，那么第i天所持有股票的最高现金就是dp[i - 1][0] + prices[i]
+         *   综上所述：
+         *   dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i])
+         * 3. dp数组如何初始化
+         *    dp[0][0] = -prices[0]
+         *    dp[0][1] = 0
+         * 4. 确定遍历顺序
+         * 5. 打印dp数组
+         */
+        int length = prices.length;
+        if (length == 0) {
+            return 0;
+        }
+        int[][] dp = new int[length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] + prices[i]);
+        }
+        return dp[length - 1][1];
     }
 }
